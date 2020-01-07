9bd2dbc604928d433c4826e1ba342fe94b1df956
Naresh <Naresh@192.168.0.26>
2020-01-07 22:51:50 +0530

    First Commit
    Uploading all Files

---
+++ myFirstProject/src/test/java/myFirstProject/test1.java
@@ -0,0 +1,13 @@
+package myFirstProject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class test1 {
+	public static void main(String[] args) {
+		System.setProperty("Webdriver.chrome.driver", "C:\\Users\\Naresh\\Downloads\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+	}
+
+}
